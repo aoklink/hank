cc7ffed4b228ce0228346b014b16b8671bfcb408
zhangyong <yong.zhang@linkfeeling.cn>
2019-07-11 14:13:40 +0800

    update

--- link_socketserve/src/main/java/cn/linkfeeling/link_socketserve/netty/MyWebSocketHandler.java
+++ link_socketserve/src/main/java/cn/linkfeeling/link_socketserve/netty/MyWebSocketHandler.java
@@ -89,8 +89,10 @@ public class MyWebSocketHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         cause.printStackTrace();
-        Log.i("link-throwable",cause.getMessage());
-      //  ctx.close();
+        Log.i("link-throwable", cause.getMessage());
+        if (!ctx.channel().isActive()) {
+            ctx.close();
+        }
     }
 
 
@@ -137,7 +139,7 @@ public class MyWebSocketHandler extends ChannelInboundHandlerAdapter {
 
         //群发，服务端向每个连接上来的客户端群发消息
         Global.group.writeAndFlush(tws);
-        }
+    }
 
     /**
      * 处理客户端向服务端发起http握手请求的业务
@@ -252,7 +254,7 @@ public class MyWebSocketHandler extends ChannelInboundHandlerAdapter {
         SmartCarProtocol body = (SmartCarProtocol) msg;
         String hostString = ((InetSocketAddress) ctx.channel().remoteAddress()).getHostString();
         Log.i("xxxxxxxxxxx", hostString);
-        socketCallBack.getBLEStream(hostString,body);
-     //   super.channelRead(ctx, msg);
+        socketCallBack.getBLEStream(hostString, body);
+        //   super.channelRead(ctx, msg);
     }
 }
