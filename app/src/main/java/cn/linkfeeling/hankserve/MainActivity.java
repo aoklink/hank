767c3afdd69db729c170acff27f57acea8e149cc
zhangyong <yong.zhang@linkfeeling.cn>
2019-07-19 17:05:53 +0800

    修改围栏数据

--- app/src/main/java/cn/linkfeeling/hankserve/MainActivity.java
+++ app/src/main/java/cn/linkfeeling/hankserve/MainActivity.java
@@ -162,8 +162,6 @@ public class MainActivity extends FrameworkBaseActivity<IUploadContract.IBleUplo
                                         value.setDistance(bigDecimal.toString());
                                     }
                                 }
-                                String s = gson.toJson(value);
-                                L.i("rrrrrrrrrrrrrrrr", s);
                                 Object clone = value.clone();
                                 if (clone != null) {
                                     tempBleInfo = (BleDeviceInfo) clone;
