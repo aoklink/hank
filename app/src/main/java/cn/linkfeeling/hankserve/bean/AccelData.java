61754214bdd20d363bc29424d7f2e8993a37ab29
zhangyong <yong.zhang@linkfeeling.cn>
2019-08-26 17:48:56 +0800

    编译so

--- app/src/main/java/cn/linkfeeling/hankserve/bean/AccelData.java
+++ app/src/main/java/cn/linkfeeling/hankserve/bean/AccelData.java
@@ -4,4 +4,28 @@ class AccelData {
     private byte x;
     private byte y;
     private byte z;
+
+    public byte getX() {
+        return x;
+    }
+
+    public void setX(byte x) {
+        this.x = x;
+    }
+
+    public byte getY() {
+        return y;
+    }
+
+    public void setY(byte y) {
+        this.y = y;
+    }
+
+    public byte getZ() {
+        return z;
+    }
+
+    public void setZ(byte z) {
+        this.z = z;
+    }
 }
--- app/src/main/java/cn/linkfeeling/hankserve/bean/WatchData.java
+++ app/src/main/java/cn/linkfeeling/hankserve/bean/WatchData.java
@@ -3,5 +3,11 @@ package cn.linkfeeling.hankserve.bean;
 class WatchData {
     public AccelData[] data;
 
+    public AccelData[] getData() {
+        return data;
+    }
 
+    public void setData(AccelData[] data) {
+        this.data = data;
+    }
 }
