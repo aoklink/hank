11143acaed1be9c0ee49e6b04fc741a348473581
zhangyong <yong.zhang@linkfeeling.cn>
2019-10-09 17:23:48 +0800

    力量器械上下左右各扩大20cm

--- app/src/main/java/cn/linkfeeling/hankserve/manager/LinkDataManager.java
+++ app/src/main/java/cn/linkfeeling/hankserve/manager/LinkDataManager.java
@@ -37,7 +37,8 @@ import static cn.linkfeeling.hankserve.constants.LinkConstant.INTERVAL_TIME;
  * @time 2019/3/14
  */
 public class LinkDataManager {
-    private static final int ExpandRange = 100;
+    private static final int ExpandRange_100 = 100;
+    private static final int ExpandRange_20 = 20;
     private static final String FIRST_LEVEL = "json";
     private static final String SECOND_LEVEL = BuildConfig.PROJECT_NAME;
     private static final String SUFFIX = ".json";
@@ -283,19 +284,19 @@ public class LinkDataManager {
                 UWBCoordData.FencePoint.Point C = fencePoint.getLeft_bottom();
                 UWBCoordData.FencePoint.Point D = fencePoint.getRight_bottom();
                 if ("跑步机".equals(devicesDatum.getType()) || "椭圆机".equals(devicesDatum.getType())) {
-                    final double a = ((B.x - ExpandRange) - (A.x + ExpandRange)) * (y - A.y) - (B.y - A.y) * (x - (A.x + ExpandRange));
-                    final double b = ((C.x - ExpandRange) - (B.x - ExpandRange)) * (y - B.y) - (C.y - B.y) * (x - (B.x - ExpandRange));
-                    final double c = ((D.x + ExpandRange) - (C.x - ExpandRange)) * (y - C.y) - (D.y - C.y) * (x - (C.x - ExpandRange));
-                    final double d = ((A.x + ExpandRange) - (D.x + ExpandRange)) * (y - D.y) - (A.y - D.y) * (x - (D.x + ExpandRange));
+                    final double a = ((B.x - ExpandRange_100) - (A.x + ExpandRange_100)) * (y - A.y) - (B.y - A.y) * (x - (A.x + ExpandRange_100));
+                    final double b = ((C.x - ExpandRange_100) - (B.x - ExpandRange_100)) * (y - B.y) - (C.y - B.y) * (x - (B.x - ExpandRange_100));
+                    final double c = ((D.x + ExpandRange_100) - (C.x - ExpandRange_100)) * (y - C.y) - (D.y - C.y) * (x - (C.x - ExpandRange_100));
+                    final double d = ((A.x + ExpandRange_100) - (D.x + ExpandRange_100)) * (y - D.y) - (A.y - D.y) * (x - (D.x + ExpandRange_100));
                     if ((a > 0 && b > 0 && c > 0 && d > 0) || (a < 0 && b < 0 && c < 0 && d < 0)) {
                         uwbCoordData.setDevice(devicesDatum);
                         writeQueue(uwbCoordData);
                     }
                 } else {
-                    final double a = (B.x - A.x) * (y - A.y) - (B.y - A.y) * (x - A.x);
-                    final double b = (C.x - B.x) * (y - B.y) - (C.y - B.y) * (x - B.x);
-                    final double c = (D.x - C.x) * (y - C.y) - (D.y - C.y) * (x - C.x);
-                    final double d = (A.x - D.x) * (y - D.y) - (A.y - D.y) * (x - D.x);
+                    final double a = ((B.x-ExpandRange_20) - (A.x+ExpandRange_20)) * (y - (A.y-ExpandRange_20)) - ((B.y-ExpandRange_20) - (A.y-ExpandRange_20)) * (x - (A.x+ExpandRange_20));
+                    final double b = ((C.x-ExpandRange_20) - (B.x-ExpandRange_20)) * (y - (B.y-ExpandRange_20)) - ((C.y+ExpandRange_20) - (B.y-ExpandRange_20)) * (x - (B.x-ExpandRange_20));
+                    final double c = ((D.x+ExpandRange_20) - (C.x-ExpandRange_20)) * (y - (C.y+ExpandRange_20)) - ((D.y+ExpandRange_20) - (C.y+ExpandRange_20)) * (x - (C.x-ExpandRange_20));
+                    final double d = ((A.x+ExpandRange_20) - (D.x+ExpandRange_20)) * (y - (D.y+ExpandRange_20)) - ((A.y-ExpandRange_20) - (D.y+ExpandRange_20)) * (x - (D.x+ExpandRange_20));
                     if ((a > 0 && b > 0 && c > 0 && d > 0) || (a < 0 && b < 0 && c < 0 && d < 0)) {
                         uwbCoordData.setDevice(devicesDatum);
                         writeQueue(uwbCoordData);
@@ -369,18 +370,18 @@ public class LinkDataManager {
             double x = newU.getX();
             double y = newU.getY();
             if ("跑步机".equals(old.getDevice().getType()) || "椭圆机".equals(old.getDevice().getType())) {
-                final double a = ((B.x - ExpandRange) - (A.x + ExpandRange)) * (y - A.y) - (B.y - A.y) * (x - (A.x + ExpandRange));
-                final double b = ((C.x - ExpandRange) - (B.x - ExpandRange)) * (y - B.y) - (C.y - B.y) * (x - (B.x - ExpandRange));
-                final double c = ((D.x + ExpandRange) - (C.x - ExpandRange)) * (y - C.y) - (D.y - C.y) * (x - (C.x - ExpandRange));
-                final double d = ((A.x + ExpandRange) - (D.x + ExpandRange)) * (y - D.y) - (A.y - D.y) * (x - (D.x + ExpandRange));
+                final double a = ((B.x - ExpandRange_100) - (A.x + ExpandRange_100)) * (y - A.y) - (B.y - A.y) * (x - (A.x + ExpandRange_100));
+                final double b = ((C.x - ExpandRange_100) - (B.x - ExpandRange_100)) * (y - B.y) - (C.y - B.y) * (x - (B.x - ExpandRange_100));
+                final double c = ((D.x + ExpandRange_100) - (C.x - ExpandRange_100)) * (y - C.y) - (D.y - C.y) * (x - (C.x - ExpandRange_100));
+                final double d = ((A.x + ExpandRange_100) - (D.x + ExpandRange_100)) * (y - D.y) - (A.y - D.y) * (x - (D.x + ExpandRange_100));
                 if ((a > 0 && b > 0 && c > 0 && d > 0) || (a < 0 && b < 0 && c < 0 && d < 0)) {
                     return true;
                 }
             } else {
-                final double a = (B.x - A.x) * (y - A.y) - (B.y - A.y) * (x - A.x);
-                final double b = (C.x - B.x) * (y - B.y) - (C.y - B.y) * (x - B.x);
-                final double c = (D.x - C.x) * (y - C.y) - (D.y - C.y) * (x - C.x);
-                final double d = (A.x - D.x) * (y - D.y) - (A.y - D.y) * (x - D.x);
+                final double a = ((B.x-ExpandRange_20) - (A.x+ExpandRange_20)) * (y - (A.y-ExpandRange_20)) - ((B.y-ExpandRange_20) - (A.y-ExpandRange_20)) * (x - (A.x+ExpandRange_20));
+                final double b = ((C.x-ExpandRange_20) - (B.x-ExpandRange_20)) * (y - (B.y-ExpandRange_20)) - ((C.y+ExpandRange_20) - (B.y-ExpandRange_20)) * (x - (B.x-ExpandRange_20));
+                final double c = ((D.x+ExpandRange_20) - (C.x-ExpandRange_20)) * (y - (C.y+ExpandRange_20)) - ((D.y+ExpandRange_20) - (C.y+ExpandRange_20)) * (x - (C.x-ExpandRange_20));
+                final double d = ((A.x+ExpandRange_20) - (D.x+ExpandRange_20)) * (y - (D.y+ExpandRange_20)) - ((A.y-ExpandRange_20) - (D.y+ExpandRange_20)) * (x - (D.x+ExpandRange_20));
                 if ((a > 0 && b > 0 && c > 0 && d > 0) || (a < 0 && b < 0 && c < 0 && d < 0)) {
                     return true;
                 }
