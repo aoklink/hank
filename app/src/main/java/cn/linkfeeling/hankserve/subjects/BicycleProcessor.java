952339e83989c0cfaa6631dd16d6c49f50c26123
zhangyong <yong.zhang@linkfeeling.cn>
2019-10-17 16:26:51 +0800

    修改单车序列号顺序

--- app/src/main/java/cn/linkfeeling/hankserve/subjects/BicycleProcessor.java
+++ app/src/main/java/cn/linkfeeling/hankserve/subjects/BicycleProcessor.java
@@ -67,7 +67,7 @@ public class BicycleProcessor implements IDataAnalysis {
 
         Log.i("danchedata", Arrays.toString(serviceData));
 
-        byte[] seqNum = {serviceData[5], serviceData[4]};
+        byte[] seqNum = {serviceData[4], serviceData[5]};
 
         if (CalculateUtil.byteArrayToInt(seqNum) < flag && flag - CalculateUtil.byteArrayToInt(seqNum) < 10000) {
             return null;
@@ -137,8 +137,8 @@ public class BicycleProcessor implements IDataAnalysis {
 
 
         byte[] ticks = new byte[2];
-        ticks[0] = serviceData[3];
-        ticks[1] = serviceData[2];
+        ticks[0] = serviceData[2];
+        ticks[1] = serviceData[3];
 
         float speed;
         if (CalculateUtil.byteArrayToInt(ticks) == 0) {
